c719ef0da2e115f078a2934bc50af5c8c8e82c4f
RichardEsquivel <rlesquivel@gmail.com>
2020-03-11 17:46:05 -0500

    done with 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -13,10 +13,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # with open takes two commands by default it goes with r for read, can capture this return of data in a variable and print out
 with open('foo.txt') as file:
     i_can_read = file.read()
-    print(i_can_read)
+    print(i_can_read, "\n")
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+# YOUR CODE HERE the w commands writes to text
+with open("bar.txt", "w") as file:
+    file.write("Man text is fun! \n")
+    file.write("Still super fun!!! \n")
+    file.write("Getting less fun now.... \n")
+
+# now will look at what we wrote by printing it out.
+with open("bar.txt") as file:
+    print(file.read())
