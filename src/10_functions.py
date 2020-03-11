4bc97d106ab42fb19c536888b6d0e7f81098d8fd
RichardEsquivel <rlesquivel@gmail.com>
2020-03-11 00:42:03 -0500

    Completed 10

--- src/10_functions.py
+++ src/10_functions.py
@@ -5,8 +5,10 @@
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+if num % 2 == 0:
+    print("Even!")
+elif num % 2 == 1:
+    print("Odd!")
